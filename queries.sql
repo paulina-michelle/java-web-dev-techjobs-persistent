7da7b281a18ca85a253975cfcf0d93be48ce8e83
Paulina <paulinah09@gmail.com>
2021-07-14 11:02:13 -0500

    completed part#1 of queries.sql - created job table

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 ## Part 1: Test it with SQL
+CREATE TABLE job (
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR
+employer VARCHAR (25)
+location VARCHAR
+skills VARCHAR (500)
+)
 
 ## Part 2: Test it with SQL
 
